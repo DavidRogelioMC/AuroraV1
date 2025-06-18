5f0df545cf362b30900d9023e579076a58e5a03b
AURORA-BOTIEF <aurorabotief@gmail.com>
2025-06-18 17:45:49 -0600

    Delete src/components/Examen.jsx

--- src/components/Examen.jsx
+++
@@ -1,8 +0,0 @@
-export default function Examen() {
-  return (
-    <div style={{ padding: '20px' }}>
-      <h2>🔬 Examen</h2>
-      <p>Esta sección mostrará tu examen final interactivo.</p>
-    </div>
-  );
-}
